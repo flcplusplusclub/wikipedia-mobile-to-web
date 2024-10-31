1117d872c2825f47f043d95b9978e4926deb53da
flcplusplusclub <flcplusplusclub@gmail.com>
2024-10-30 23:17:58 -0700

    Added Functionality and Icons

---
+++ script.js
@@ -0,0 +1,4 @@
+if (window.location.href.includes(".m.wikipedia")) 
+{
+	window.location.href = window.location.href.replace(".m.wikipedia", ".wikipedia");
+}
--- scripts/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-console.log(window.location.toString());
-console.log("working");
-});
